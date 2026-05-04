36126a79846546c7b3ccba87bbd31224d1012c5b
Benjamin-Ettey <benjaminettey2005@gmail.com>
2026-05-04 16:04:39 +0300

    Changed fonts to GoogleSans and Poppins

--- driver-app/Troski-Driver/app/_layout.tsx
+++ driver-app/Troski-Driver/app/_layout.tsx
@@ -4,16 +4,16 @@ import {useFonts} from "expo-font";
 import "../global.css"
 
 
+
 const _Layout = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const [loaded] = useFonts({
-        "Manrope-Bold": require("../assets/fonts/Manrope-Bold.ttf"),
-        "Manrope-ExtraBold": require("../assets/fonts/Manrope-ExtraBold.ttf"),
-        "Manrope-ExtraLight": require("../assets/fonts/Manrope-ExtraLight.ttf"),
-        "Manrope-Light": require("../assets/fonts/Manrope-Light.ttf"),
-        "Manrope-Medium": require("../assets/fonts/Manrope-Medium.ttf"),
-        "Manrope-Regular": require("../assets/fonts/Manrope-Regular.ttf"),
-        "Manrope-SemiBold": require("../assets/fonts/Manrope-SemiBold.ttf"),
+        "GoogleSans-Bold": require("../assets/fonts/GoogleSans-Bold.ttf"),
+        "GoogleSans-Medium": require("../assets/fonts/GoogleSans-Medium.ttf"),
+        "GoogleSans-Regular": require("../assets/fonts/GoogleSans-Regular.ttf"),
+        "GoogleSans-SemiBold": require("../assets/fonts/GoogleSans-SemiBold.ttf"),
     });
+    if (!loaded) return null;
 
     return (
         <Stack/>
